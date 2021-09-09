11316db22da0d4172bbdd6b672506ee9d232752f
Krishna Kaushik <46679308+krishnakaushik25@users.noreply.github.com>
2021-09-09 10:02:59 +0530

    Update main.py

--- neural_style/main.py
+++ neural_style/main.py
@@ -21,9 +21,9 @@ style_name = st.sidebar.selectbox(
 )
 
 
-model= "saved_models/" + style_name + ".pth"
-input_image = "images/content-images/" + img
-output_image = "images/output-images/" + style_name + "-" + img
+model= "/app/streamlit-style-transfer/neural_style/saved_models/" + style_name + ".pth"
+input_image = "/app/streamlit-style-transfer/neural_style/images/content-images/" + img
+output_image = "/app/streamlit-style-transfer/neural_style/images/output-images/" + style_name + "-" + img
 
 st.write('### Source image:')
 image = Image.open(input_image)
